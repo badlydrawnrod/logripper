022ae7c798d30cf5c54a1ee05a9437077021caef
Rod <rod@badlydrawngames.com>
2021-07-06 16:36:21 +0100

    Tidy code.

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ class TextStream:
     def peekline(self):
         if len(self.current_line) == 0:
             self.current_line = self.reader.readline()
-            matched = iso_re.match(self.current_line)
-            if matched:
+            if matched := iso_re.match(self.current_line):
                 start, end = matched.span()
                 self.current_time = dateutil.parser.parse(self.current_line[start:end])
                 self.current_line = self.current_line[end:]
@@ -50,34 +49,32 @@ def guess_encoding(open_fn, lines=10):
                 continue
 
 
-def directory_storage_iterator(path):
+def recurse_in_directory(path):
     result = []
     for full_path in sorted(path.glob("**/*")):
         if full_path.is_file():
             # TODO: remove this hack and go and figure out what the file is.
             if str(full_path).endswith(".zip"):
                 with zipfile.ZipFile(full_path) as f:
-                    result.extend(zip_storage_iterator(full_path, f))
+                    result.extend(recurse_in_zip(full_path, f))
             else:
                 open_fn = lambda: open(full_path, "rb")
-                encoding = guess_encoding(open_fn)
-                if encoding:
+                if encoding := guess_encoding(open_fn):
                     result.append(TextStream(io.BufferedReader(open_fn()), full_path, encoding))
     return result
 
 
-def zip_storage_iterator(parent_path, zf):
+def recurse_in_zip(parent_path, zf):
     result = []
     for info in zf.infolist():
         # TODO: remove this hack and go and figure out what the file is.
         full_path = parent_path / info.filename
         if info.filename.endswith(".zip"):
             with zipfile.ZipFile(zf.open(info)) as f:
-                result.extend(zip_storage_iterator(full_path, f))
+                result.extend(recurse_in_zip(full_path, f))
         else:
             open_fn = lambda: zf.open(info)
-            encoding = guess_encoding(open_fn)
-            if encoding:
+            if encoding := guess_encoding(open_fn):
                 result.append(TextStream(io.BufferedReader(open_fn()), full_path, encoding))
     return result
 
@@ -85,33 +82,27 @@ def zip_storage_iterator(parent_path, zf):
 if __name__ == "__main__":
     path = pathlib.Path(".")
 
-    text_streams = directory_storage_iterator(path)
+    text_streams = recurse_in_directory(path)
 
     # Advance each stream until there are no streams left.
     while len(text_streams) > 0:
-        # Find the stream with the oldest timestamp, and remove streams that don't have timestamps.
-        oldest = None
-        streams = []
-        for stream in text_streams:
-            line = stream.peekline()
-            if stream.current_time is not None:
-                streams.append(stream)
-                if oldest is None or stream.current_time < oldest.current_time:
-                    oldest = stream
-        text_streams = streams
+        # Order the streams by timestamp, removing those that have none.
+        text_streams = [stream for stream in text_streams if stream.peekline() and stream.current_time is not None]
+        text_streams.sort(key=lambda s: s.current_time)
 
-        if oldest is not None:
+        # Output the oldest stream.
+        if len(text_streams) > 0:
+            oldest = text_streams[0]
             line = oldest.readline()
             ts = oldest.current_time.isoformat()
-            if len(line) > 0:
+            line = line.strip()
+            print(f"{ts}, {line}, {oldest.path}")
+
+            # Read any lines from the oldest stream that don't have a timestamp and account for them as if they're part
+            # of the previous line.
+            line = oldest.peekline()
+            while len(line) > 0 and oldest.current_time is None:
+                line = oldest.readline()
                 line = line.strip()
                 print(f"{ts}, {line}, {oldest.path}")
-
-                # Read any lines that don't have a timestamp and account for them as if they're part of the previous
-                # line.
                 line = oldest.peekline()
-                while len(line) > 0 and oldest.current_time is None:
-                    line = oldest.readline()
-                    line = line.strip()
-                    print(f"{ts}, {line}, {oldest.path}")
-                    line = oldest.peekline()
