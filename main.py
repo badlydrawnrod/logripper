472693a49bde9cc27acafe38f828cf05bf5d6dea
Rod <rod@badlydrawngames.com>
2021-07-06 14:20:22 +0100

    Get rid of global.

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import io
 import pathlib
 import zipfile
 
-all_streams = []
-
 
 class TextStream:
     def __init__(self, owner, path, encoding):
@@ -30,42 +28,47 @@ def guess_encoding(open_fn, lines=10):
 
 
 def directory_storage_iterator(path):
+    result = []
     for full_path in sorted(path.glob("**/*")):
         if full_path.is_file():
             # TODO: remove this hack and go and figure out what the file is.
             if str(full_path).endswith(".zip"):
                 with zipfile.ZipFile(full_path) as f:
-                    zip_storage_iterator(full_path, f)
+                    result.extend(zip_storage_iterator(full_path, f))
             else:
                 open_fn = lambda: open(full_path, "rb")
                 encoding = guess_encoding(open_fn)
                 if encoding:
-                    all_streams.append(TextStream(io.BufferedReader(open_fn()), full_path, encoding))
+                    result.append(TextStream(io.BufferedReader(open_fn()), full_path, encoding))
+    return result
 
 
 def zip_storage_iterator(parent_path, zf):
+    result = []
     for info in zf.infolist():
         # TODO: remove this hack and go and figure out what the file is.
         full_path = parent_path / info.filename
         if info.filename.endswith(".zip"):
             with zipfile.ZipFile(zf.open(info)) as f:
-                zip_storage_iterator(full_path, f)
+                result.extend(zip_storage_iterator(full_path, f))
         else:
             open_fn = lambda: zf.open(info)
             encoding = guess_encoding(open_fn)
             if encoding:
-                all_streams.append(TextStream(io.BufferedReader(open_fn()), full_path, encoding))
+                result.append(TextStream(io.BufferedReader(open_fn()), full_path, encoding))
+    return result
 
 
 if __name__ == "__main__":
     path = pathlib.Path("samples")
 
-    directory_storage_iterator(path)
+    text_streams = directory_storage_iterator(path)
 
+    # Advance each stream by one line until no stream can be advanced.
     read_data = True
     while read_data:
         read_data = False
-        for stream in all_streams:
+        for stream in text_streams:
             line = stream.readline()
             if len(line) > 0:
                 print(f"PATH {stream.path} ENCODING {stream.encoding} LINE {line.rstrip()}")
