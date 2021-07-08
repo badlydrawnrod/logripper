54835385914ea58bf793920fbc61431f9798d775
Rod <rod@badlydrawngames.com>
2021-07-08 14:22:15 +0100

    Explicitly close streams once done. Refactor / rename.

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ import dateutil.parser
 # TODO: associate the time format with the stream so that we don't have to keep working it out.
 # TODO: add a requirements.txt, or whatever Python uses these days.
 # TODO: better error handling all round.
-# TODO: explicitly close files.
+# TODO: add a licence.
 
 # Compile a regular expression that will match an ISO timestamp.
 iso = r"\d{4}-\d{2}-\d{2}[T ]\d{2}:\d{2}:\d{2}(\.(\d{9}|\d{6}|\d{3}))?(Z|[+-]\d{4}|[+-]\d{2}(:\d{2})?)?"
 iso_re = re.compile(iso)
 
 
-class TextStream:
+class LogStream:
     def __init__(self, owner, path, encoding):
-        self.owner = owner
-        self.reader = io.TextIOWrapper(self.owner, encoding=encoding)
+        self.reader = io.TextIOWrapper(owner, encoding=encoding)
         self.path = path
         self.current_line = ""
         self.current_time = None
@@ -45,6 +44,9 @@ class TextStream:
         self.current_line = ""
         return result
 
+    def close(self):
+        self.reader.close()
+
 
 def guess_encoding(open_fn, lines=10):
     encodings = ["utf-16", "utf-8", "ascii"]
@@ -65,14 +67,14 @@ def guess_encoding(open_fn, lines=10):
         # tar file for which there is an entry, but nothing extractable for that entry.
 
 
-def as_text_stream(open_fn, full_path):
+def open_as_log(open_fn, full_path):
     if encoding := guess_encoding(open_fn):
-        text_stream = TextStream(io.BufferedReader(open_fn()), full_path, encoding)
-        if text_stream.current_time is not None:
-            return text_stream
+        stream = LogStream(io.BufferedReader(open_fn()), full_path, encoding)
+        if stream.current_time is not None:
+            return stream
 
 
-def as_zip(open_token):
+def open_as_zip(open_token):
     try:
         return zipfile.ZipFile(open_token())
     except zipfile.BadZipfile:
@@ -81,7 +83,7 @@ def as_zip(open_token):
         pass
 
 
-def as_tar(open_token):
+def open_as_tar(open_token):
     try:
         return tarfile.open(open_token())
     except tarfile.TarError:
@@ -93,19 +95,35 @@ def as_tar(open_token):
 
 
 def recurse(open_fn, full_path, open_token):
-    if f := as_zip(open_token):
+    if f := open_as_zip(open_token):
         try:
             return recurse_in_zip(full_path, f)
         finally:
             f.close()
-    elif f := as_tar(open_token):
+    elif f := open_as_tar(open_token):
         try:
             return recurse_in_tar(full_path, f)
         finally:
             f.close()
-    elif text_stream := as_text_stream(open_fn, full_path):
-        return [text_stream]
+    elif stream := open_as_log(open_fn, full_path):
+        return [stream]
     return []
+
+
+def recurse_in_zip(parent_path, zf):
+    result = []
+    for info in zf.infolist():
+        full_path = parent_path / info.filename
+        result.extend(recurse(lambda: zf.open(info), full_path, lambda: zf.open(info)))
+    return result
+
+
+def recurse_in_tar(parent_path, tf):
+    result = []
+    for info in tf.getmembers():
+        full_path = parent_path / info.name
+        result.extend(recurse(lambda: tf.extractfile(info), full_path, lambda: tf.extractfile(info)))
+    return result
 
 
 def recurse_in_filesystem(filepaths):
@@ -125,52 +143,40 @@ def recurse_in_filesystem(filepaths):
     return result
 
 
-def recurse_in_zip(parent_path, zf):
+def remove_finished_streams(streams):
     result = []
-    for info in zf.infolist():
-        full_path = parent_path / info.filename
-        result.extend(recurse(lambda: zf.open(info), full_path, lambda: zf.open(info)))
+    for stream in streams:
+        if stream.peekline() and stream.current_time is not None:
+            result.append(stream)
+        else:
+            stream.close()
+    result.sort(key=lambda s: s.current_time)
     return result
 
 
-def recurse_in_tar(parent_path, tf):
-    result = []
-    for info in tf.getmembers():
-        full_path = parent_path / info.name
-        result.extend(recurse(lambda: tf.extractfile(info), full_path, lambda: tf.extractfile(info)))
-    return result
+def main(filepaths):
+    streams = recurse_in_filesystem(filepaths)
 
+    while streams := remove_finished_streams(streams):
+        oldest = streams[0]
+        line = oldest.readline()
+        timestamp = oldest.current_time.isoformat()
+        line = line.strip()
+        print(f"{timestamp}, {line}, {oldest.path}")
 
-def main(filepath):
-    text_streams = recurse_in_filesystem(filepath)
-
-    while len(text_streams) > 0:
-        # Order the streams by timestamp, removing those that have none.
-        text_streams = [stream for stream in text_streams if stream.peekline() and stream.current_time is not None]
-        text_streams.sort(key=lambda s: s.current_time)
-
-        # Read from the oldest stream.
-        if len(text_streams) > 0:
-            oldest = text_streams[0]
+        # Read any lines from the oldest stream that don't have a timestamp and output them as if they have the same
+        # timestamp as the previous line.
+        line = oldest.peekline()
+        while len(line) > 0 and oldest.current_time is None:
             line = oldest.readline()
-            timestamp = oldest.current_time.isoformat()
             line = line.strip()
             print(f"{timestamp}, {line}, {oldest.path}")
-
-            # Read any lines from the oldest stream that don't have a timestamp and account for them as if they have
-            # the same timestamp as the previous line.
             line = oldest.peekline()
-            while len(line) > 0 and oldest.current_time is None:
-                line = oldest.readline()
-                line = line.strip()
-                print(f"{timestamp}, {line}, {oldest.path}")
-                line = oldest.peekline()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Chomp through log files")
+    parser = argparse.ArgumentParser(description="Chomp through log files.")
     parser.add_argument("paths", metavar="file/dir", type=str, nargs="*", help="filenames or directories to search")
     args = parser.parse_args()
 
-    paths = [pathlib.Path(p) for p in args.paths]
-    main(paths)
+    main(pathlib.Path(p) for p in args.paths)
